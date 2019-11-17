b725478772d7d992e0fe56c2d3343281090bd0ee
smakosh <ismai23l@hotmail.com>
2019-11-17 19:39:46 +0100

    chore: done with the new Docs website

--- docz.js
+++ docz.js
@@ -1,3 +1,24 @@
 export default {
-  public: "/public"
+  public: "/public",
+  menu: [
+    '1. Introduction',
+    '2. Getting Started',
+    {
+      name: 'Components',
+      menu: [
+        'Buttons',
+        'Cards',
+        'Forms'
+      ]
+    },
+    {
+      name: 'Layout',
+      menu: [
+        'Containers',
+        'Grid',
+        'Media'
+      ]
+    },
+    'Utilities'
+  ]
 };
